f4a6ca6634b5018aa651a83481a071af9885ea35
Siarhei Filinski <sfilisnky@gmail.com>
2025-09-16 17:09:14 +0200

    Introduce macro to generate exporters for components.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+pub extern crate self as bevy_godot4;
+pub use godot;
+
 mod app;
 #[cfg(feature = "assets")]
 mod assets;
@@ -6,7 +9,10 @@ mod scene;
 mod scene_tree;
 mod utils;
 mod app_action_queue;
-mod tools;
+pub mod tools;
+pub use tools::collect_children;
+
+pub use bevy_godot4_proc_macros::ExportToGodot;
 
 pub mod prelude {
     pub use super::erased_gd::{ErasedGd, ErasedGdResource};
@@ -15,8 +21,11 @@ pub mod prelude {
     pub use super::utils::{
         AsPhysicsSystem, AsVisualSystem, GodotPhysicsFrame, GodotVisualFrame, SystemDeltaTimer,
     };
-    pub use bevy_godot4_proc_macros::bevy_app;
+    pub use bevy_godot4_proc_macros::{
+        bevy_app,
+    };
+    pub use crate::ExportToGodot;
     pub use super::app_action_queue::{AppAction};
-    pub use super::tools::{collect_children};
+    pub use crate::{collect_children};
 }
 pub use app::{BevyApp, APP_BUILDER_FN};
