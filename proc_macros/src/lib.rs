13b29bbce5c438c0f74476b34c11e36d108b8403
Siarhei Filinski <sfilisnky@gmail.com>
2026-03-25 18:38:04 +0100

    Remove export_bundle macro.

--- proc_macros/src/lib.rs
+++ proc_macros/src/lib.rs
@@ -1,5 +1,4 @@
 mod bevy_app_impl;
-mod export_bundle;
 mod export_component_impl;
 mod export_component_new_impl;
 mod export_composed;
@@ -28,11 +27,6 @@ pub fn export_component_new(input: TokenStream) -> TokenStream {
 #[proc_macro_derive(ExportEntity, attributes(export_entity))]
 pub fn export_entity(input: TokenStream) -> TokenStream {
     export_entity_impl::expand(input)
-}
-
-#[proc_macro]
-pub fn export_bundle(input: TokenStream) -> TokenStream {
-    export_bundle::expand(input)
 }
 
 #[proc_macro]
